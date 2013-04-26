754063c59585fd156316f763d13cfacf55095c01
Stasevi4 <stas19882004@gmail.com>
2013-04-26 14:49:11 -0300

    Update Toolbar.php

--- app/code/community/Stasevi4/orderby/Block/Product/List/Toolbar.php
+++ app/code/community/Stasevi4/orderby/Block/Product/List/Toolbar.php
@@ -20,6 +20,20 @@ class Orderby_Catalog_Block_Product_List_Toolbar extends Mage_Catalog_Block_Prod
         
         return $this;
     }
+    
+    public function getCurrentDirection() 
+	{ 
+		if ($dir = (string) $this->getRequest()->getParam($this->getDirectionVarName())) { 
+		
+			$dir = strtolower($dir);
+ 
+			if (in_array($dir, array('desc', 'asc'))) { 
+			    
+                return $dir; 
+			} 
+		} 
+		return 'desc'; 
+	}
   
 }
 ?>
